5b3489bcef7e6d2762a2a400ea23ff72709e6b89
datlth <152574562+datlth@users.noreply.github.com>
2024-01-30 14:03:50 +0700

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -6,22 +6,33 @@ from torch.utils.data import Dataset
 senttag2word = {'POS': 'positive', 'NEG': 'negative', 'NEU': 'neutral'}
 
 
-def read_line_examples_from_file(data_path):
+def read_line_examples_from_file(data_path, silence=False):
     """
     Read data from file, each line is: sent####labels
     Return List[List[word]], List[Tuple]
     """
-    sents, labels = [], []
-    with open(data_path, 'r', encoding='UTF-8') as fp:
+    id_users, sents, labels = [], [], []
+    with open(data_path, "r", encoding="UTF-8") as fp:
         words, labels = [], []
         for line in fp:
             line = line.strip()
-            if line != '':
-                words, tuples = line.split('####')
-                sents.append(words.split())
-                labels.append(eval(tuples))
-    print(f"Total examples = {len(sents)}")
-    return sents, labels
+            if line != "":
+                words, tuples = line.split("####")
+                if tuples != "":
+                    labels.append(eval(tuples))
+                else:
+                    words = line
+                # Get id user at the beginning of line if it exist
+                words = words.split()
+                if len(words[0].split(',', 1)) > 1:
+                    id_user, word = words[0].split(',',1)
+                    words[0] = word
+                    id_users.append(int(id_user))
+                sents.append(words)
+
+    if silence:
+        print(f"Total examples = {len(sents)}")
+    return id_users, sents, labels
 
 
 def get_annotated_uabsa_targets(sents, labels):
@@ -199,10 +210,10 @@ def get_extraction_aste_targets(sents, labels):
 
 def get_transformed_io(data_path, paradigm, task):
     """
-    The main function to transform the Input & Output according to 
+    The main function to transform the Input & Output according to
     the specified paradigm and task
     """
-    sents, labels = read_line_examples_from_file(data_path)
+    id_users, sents, labels = read_line_examples_from_file(data_path)
 
     # the input is just the raw sentence
     inputs = [s.copy() for s in sents]
@@ -234,9 +245,9 @@ def get_transformed_io(data_path, paradigm, task):
             raise NotImplementedError
     else:
         print('Unsupported paradigm!')
-        raise NotImplementedError 
+        raise NotImplementedError
 
-    return inputs, targets
+    return id_users, inputs, targets
 
 
 class ABSADataset(Dataset):
@@ -266,16 +277,18 @@ class ABSADataset(Dataset):
         return {"source_ids": source_ids, "source_mask": src_mask, 
                 "target_ids": target_ids, "target_mask": target_mask}
 
-    def _build_examples(self):
+   def _build_examples(self):
 
-        inputs, targets = get_transformed_io(self.data_path, self.paradigm, self.task)
+        id_users, inputs, targets = get_transformed_io(self.data_path, self.paradigm, self.task)
+
+        self.id_users = id_users
 
         for i in range(len(inputs)):
 
-            input = ' '.join(inputs[i]) 
+            input = ' '.join(inputs[i])
             if self.paradigm == 'annotation':
                 if self.task != 'tasd':
-                    target = ' '.join(targets[i]) 
+                    target = ' '.join(targets[i])
                 else:
                     target = targets[i]
             else:
@@ -292,6 +305,59 @@ class ABSADataset(Dataset):
 
             self.inputs.append(tokenized_input)
             self.targets.append(tokenized_target)
+
+# Predict dataloader
+class ABSADataset_nolabel(Dataset):
+    def __init__(self, tokenizer, data_dir, data_type, task, max_len=128):
+        # './data/rest16/train.txt'
+        self.data_path = data_dir
+        self.max_len = max_len
+        self.tokenizer = tokenizer
+        self.data_dir = data_dir
+
+        self.id_users = []
+        self.inputs = []
+
+        self.task = task
+
+        self._build_examples()
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        source_ids = self.inputs[index]["input_ids"].squeeze()
+
+        src_mask = self.inputs[index][
+            "attention_mask"
+        ].squeeze()  # might need to squeeze
+
+        return {
+            "source_ids": source_ids,
+            "source_mask": src_mask,
+        }
+
+    def _build_examples(self):
+        id_user, inputs, _ = get_transformed_io(
+            self.data_path, self.data_dir, self.task
+        )
+
+        check = 1 if len(id_user) > 0 else 0
+        for i in range(len(inputs)):
+            # change input and target to two strings
+            input = " ".join(inputs[i])
+
+            tokenized_input = self.tokenizer.batch_encode_plus(
+                [input],
+                max_length=self.max_len,
+                padding="max_length",
+                truncation=True,
+                return_tensors="pt",
+            )
+
+            if check:
+                self.id_users.append(id_user[i])
+            self.inputs.append(tokenized_input)
 
 
 def write_results_to_log(log_file_path, best_test_result, args, dev_results, test_results, global_steps):
